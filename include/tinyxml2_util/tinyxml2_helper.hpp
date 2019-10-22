5bf5bf034c05792d06c71ee9c7bbbc24623c8d35
Jacob Schloss <jacob@schloss.io>
2019-10-21 19:06:18 -0700

    license

--- include/tinyxml2_util/tinyxml2_helper.hpp
+++ include/tinyxml2_util/tinyxml2_helper.hpp
@@ -1,3 +1,10 @@
+/**
+ * @brief tinyxml2_helper
+ * @author Jacob Schloss <jacob@schloss.io>
+ * @copyright Copyright (c) 2019 Jacob Schloss. All rights reserved.
+ * @license Licensed under the 3-Clause BSD license. See LICENSE for details
+*/
+
 #pragma once
 
 #include "../external/tinyxml2/tinyxml2.h"
